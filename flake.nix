47c23d076f5c4f9107fd04035657fb0ef709e3d8
fusion44 <some.fusion@gmail.com>
2025-02-11 10:56:48 +0100

    feat: add a nixblitz nixos module

--- flake.nix
+++ flake.nix
@@ -1,22 +1,40 @@
 {
   description = "NixBlitz dev env";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
+
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
+  }: let
+    name = "nixblitz";
+
+    module = {
+      nixosModules = {
+        ${name} = {...}: {
+          imports = [./modules/nixblitz.nix];
+          nixpkgs.overlays = [self.overlays.default];
+        };
+        default = self.nixosModules.${name};
       };
+    };
+
+    overlays.overlays = {
+      default = final: prev: {
+        ${name} = self.packages.${prev.stdenv.hostPlatform.system}.${name};
+      };
+    };
+
+    systems = flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
     in {
       packages = {
-        nixblitz = pkgs.callPackage ./default.nix {};
-        default = self.packages.${system}.nixblitz;
+        ${name} = pkgs.callPackage ./default.nix {};
+        default = self.packages.${system}.${name};
       };
 
       devShell = with pkgs;
@@ -39,6 +57,7 @@
             just # the command runner
             nushell # alternative to Bash
             typos # code spell checker
+            statix
           ];
           nativeBuildInputs = with pkgs; [
             pkg-config
@@ -46,4 +65,6 @@
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
     });
+  in
+    overlays // module // systems;
 }
---
+++ modules/nixblitz.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  name = "nixblitz";
+  cfg = config.services.${name};
+
+  inherit (lib) mkOption mkIf types literalExpression;
+in {
+  options = {
+    services.${name} = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        description = "Enable the nixblitz package.";
+      };
+
+      package = mkOption {
+        type = types.package;
+        defaultText = literalExpression "pkgs.${name}";
+        default = pkgs.${name};
+        description = "The ${name} package to use.";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      nixblitz
+    ];
+  };
+}
