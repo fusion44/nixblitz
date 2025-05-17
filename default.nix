c21c53df3f9695e6effd04a6f9feaac7ab34c67d
fusion44 <git.bleep096@passmail.net>
2025-05-17 16:12:53 +0200

    feat: include the commit hash in version string

--- default.nix
+++ default.nix
@@ -1,14 +1,22 @@
+# default.nix
 {pkgs ? import <nixpkgs> {}}: let
   manifest = (pkgs.lib.importTOML ./packages/cli/Cargo.toml).package;
-  rev = "a2dc1b51881efdbb52f6354d51c7a253c3d31fb2";
+  commitSha = "7b38c12e6c7e276c459e3aa4bc0140750578761b";
+  shortSha = builtins.substring 0 7 commitSha;
+
   src = pkgs.fetchFromGitHub {
     owner = "fusion44";
     repo = "nixblitz";
-    inherit rev;
+    rev = commitSha;
     sha256 = "sha256-uVUzz7Msjfox9SZloyTaDANBrockaeTGCxDnrdx5PT4=";
   };
 
   crateSource = src + "/packages";
+  vergenGitSha = commitSha;
+  vergenGitDescribe = "${shortSha}-nix";
+  vergenGitDirty = "false";
+
+  vergenSourceDateEpoch = "0";
 in
   pkgs.rustPlatform.buildRustPackage {
     pname = manifest.name;
@@ -16,13 +24,16 @@ in
     src = crateSource;
 
     cargoLock.lockFile = crateSource + "/Cargo.lock";
-    VERGEN_GIT_SHA = rev;
+    VERGEN_GIT_SHA = vergenGitSha;
+    VERGEN_GIT_DESCRIBE = vergenGitDescribe;
+    VERGEN_GIT_DIRTY = vergenGitDirty;
+    SOURCE_DATE_EPOCH = vergenSourceDateEpoch;
 
     meta = {
       description = manifest.description or "Management CLI for the nixblitz project";
       homepage = manifest.homepage or "https://github.com/fusion44/nixblitz";
-      license = pkgs.lib.licenses.mit; # Assuming MIT, adjust if different
-      maintainers = ["fusion44"]; # Add your Nix maintainer handle if desired
-      mainProgram = manifest.name; # The name of the binary produced
+      license = pkgs.lib.licenses.mit;
+      maintainers = ["fusion44"];
+      mainProgram = manifest.name;
     };
   }
--- packages/cli/build.rs
+++ packages/cli/build.rs
@@ -2,10 +2,23 @@ use vergen::{BuildBuilder, Emitter};
 use vergen_git2::Git2Builder;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let _ = Emitter::default()
-        .add_instructions(&BuildBuilder::all_build()?)?
-        .add_instructions(&Git2Builder::all_git()?)?
-        .emit();
+    let mut emitter = Emitter::default();
+    emitter.add_instructions(&BuildBuilder::all_build()?)?;
+    if std::env::var("VERGEN_IDEMPOTENT").is_err() {
+        println!(
+            "cargo:warning=VERGEN_IDEMPOTENT not set, attempting to generate Git info via vergen."
+        );
+        let git_instructions = Git2Builder::default()
+            .sha(true)
+            .describe(true, true, None)
+            .dirty(true)
+            .build()?;
+        let _ = emitter.add_instructions(&git_instructions);
+    } else {
+        println!("cargo:warning=VERGEN_IDEMPOTENT is set, skipping vergen's Git info generation. Expecting Nix to provide VERGEN_GIT_SHA, etc.");
+    }
+
+    let _ = emitter.emit();
 
     Ok(())
 }
